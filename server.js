db67f79fd524bc5d68d33390a21c691beb77548b
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-08-17 16:24:35 -0500

    Set up mongoose schema

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs  = require('express-handlebars');
+var bodyParser = require("body-parser");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -11,10 +13,20 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
+
+
+//Initialize Handlebars 
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+    extended: false}
+    ));
 
 // Configure middleware
 
@@ -30,3 +42,8 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+// Start the server
+app.listen(PORT, function() {
+console.log("App running on port " + PORT + "!")
+});
